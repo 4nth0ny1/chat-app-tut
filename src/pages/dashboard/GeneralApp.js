0b822417f02d5d9dc6393af100ab568cd07dc43a
4nth0ny1 <acatullo4@gmail.com>
2023-10-14 18:08:04 -0500

    completed top info bar for conversation component

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,15 +1,26 @@
 import React, { Suspense, lazy } from "react";
+import { Stack, Box } from "@mui/material";
 import Chats from "./Chats";
+import Conversation from "../../components/Conversation/index";
 
 // Dynamic loading
 const Cat = lazy(() => import("../../components/Cat"));
 
 const GeneralApp = () => {
   return (
-    <>
-      {/* <h2>App Component</h2> */}
+    <Stack direction="row" sx={{ width: "100%" }}>
       <Chats />
-    </>
+
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor: "#fff",
+        }}
+      >
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
