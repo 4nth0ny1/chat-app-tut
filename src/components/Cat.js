e153f0dc08b53dc7bed0067375af9e1f02f093df
4nth0ny1 <acatullo4@gmail.com>
2023-10-13 20:46:51 -0500

    overview of routing and added cat avatar

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Avatar } from "@mui/material";
+
+const Cat = () => {
+  return (
+    <Avatar src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60" />
+  );
+};
+
+export default Cat;
